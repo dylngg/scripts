327e91beebd1f4c025ad472f60d36813e0a25404
dylngg <dylan@gardnermedia.com>
2020-09-05 12:26:21 -0600

    Make md2pdf script work with both .markdown and .md

--- md2pdf.sh
+++ md2pdf.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 # A wrapper around pandoc for sane markdown to pdf conversion
-pandoc -s $1 -V geometry:margin=1in --mathjax -o `basename $1 .md`.pdf
+original="$1"
+if [ "$(basename $original .md)" != "$original" ]; then
+    prefix="$(basename $original .md)"
+elif [ "$(basename $original .markdown)" != "$original" ]; then
+    prefix="$(basename $original .markdown)"
+else
+    echo "File given does not end in .md or .markdown." > /dev/stderr
+    exit 1
+fi
+
+pandoc -s "$original" -f markdown -o "$prefix.pdf" -V geometry:margin=1in -V colorlinks:true
