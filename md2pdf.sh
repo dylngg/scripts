bbc689413fa9e118c5434371d404d072a898725d
dylngg <dylan@gardnermedia.com>
2021-01-11 18:24:32 -0700

    Quote incoming file arguments to prevent erroneous expansion
    Mhmm. Shells are quite fickle creatures...

--- md2pdf.sh
+++ md2pdf.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # A wrapper around pandoc for sane markdown to pdf conversion
 original="$1"
-if [ "$(basename $original .md)" != "$original" ]; then
-    prefix="$(basename $original .md)"
-elif [ "$(basename $original .markdown)" != "$original" ]; then
-    prefix="$(basename $original .markdown)"
+if [ "$(basename "$original" .md)" != "$original" ]; then
+    prefix="$(basename "$original" .md)"
+elif [ "$(basename "$original" .markdown)" != "$original" ]; then
+    prefix="$(basename "$original" .markdown)"
 else
     echo "File given does not end in .md or .markdown." > /dev/stderr
     exit 1
--- nsepoch2date.sh
+++ nsepoch2date.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Given nanoseconds in epoch as the first argument, prints out a nicely
 # formatted date for the given epoch.
-if [ -z "$1" ]; then
+if [ "$1" != "" ]; then
     echo "No ns timestamp given." > /dev/stderr
     exit 1
 fi
