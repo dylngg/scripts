3e2697dfd06df4b7fc81cce7a22b92af4f9f7aa7
dylngg <dylan@gardnermedia.com>
2021-02-02 17:08:42 -0700

    Tweak downhere.sh to new copydownhere.sh that copies, rather than moves

--- downhere.sh
+++ downhere.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-# Moves the last downloaded item in ~/Downloads to the current directory or
-# the directory specified
+# Copies or moves the last downloaded item in ~/Downloads to the current
+# directory or the directory specified. Copies when $0 is "copydownhere".
+cmd="mv"
+script="$(basename "$0")"
+if [ "$script" = "copydownhere" ] || [ "$script" = "copydownhere.sh" ]; then
+    cmd="cp"
+fi
 here="."
 if [ "$1" != "" ]; then
     here="$1"
 fi
 there="$HOME/Downloads"
 last="$(ls -t1 "$there" | head -1)"
-mv "$there/$last" "$here"
+"$cmd" "$there/$last" "$here"
