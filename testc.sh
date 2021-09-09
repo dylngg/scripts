5ee789c742eaad5712d41e41d2dbbd8bd3453619
Dylan Gardner <dylan.gardner@utah.edu>
2021-09-08 20:23:34 -0600

    Make testc.sh produce C++ files when 'testc++' is used

--- testc.sh
+++ testc.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-# Generates a template c file for testing
-cat <<EOF | vim - -c 'set filetype=c'
+# Generates a template c or c++ file for testing
+script_name="$(basename "$0")"
+if [ "$script_name" = "testc++" ] || [ "$script_name" = "testc++.sh" ]; then
+    cat <<EOF | vim - -c 'set filetype=cpp'
+#include <iostream>
+
+int main(int argc, char **argv) {
+    std::cout << 1 << std::endl;
+}
+EOF
+
+else
+    cat <<EOF | vim - -c 'set filetype=c'
 #include "stdio.h"
 
 int main(int argc, char **argv) {
     printf("%d\n", 1);
 }
 EOF
+
+fi
+
