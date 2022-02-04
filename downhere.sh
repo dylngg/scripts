cbe146cff42c99ef891e6e8d21f3dbfe204ddd7a
Dylan Gardner <dylan.gardner@utah.edu>
2022-02-03 19:34:21 -0700

    Combine dochere and downhere into one file

--- downhere.sh
+++ downhere.sh
@@ -1,20 +1,50 @@
-#!/bin/sh
-# Copies or moves the last downloaded item in ~/Downloads to the current
-# directory or the location specified. Copies when $0 is "copydownhere".
-cmd="mv"
+#!/bin/bash
+# Copies or moves the last item (modtime) in ~/Downloads or ~/Documents to the
+# current directory or the destination specified.
+#
+# Prints the last item, rather than copying/moving, when $0 does not end with "here"
+# Uses ~/Documents, rather than ~/Downloads, when $0 contains "doc" instead of "down".
+# Copies, rather than moves, when $0 starts with "copy".
+#
+# Usage: downhere [destdir]             # moves from ~/Downloads
+#     or copydownhere [destdir]         # copies from ~/Downloads
+#     or dochere [destdir]              # moves from ~/Documents
+#     or copydochere [destdir]          # copies from ~/Documents
+#     or down                           # Prints the last item in ~/Downloads
+#     or doc                            # Prints the last item in ~/Documents
+set -e
+
+source="$HOME/Downloads"
+cmd="echo"
+dest=""
+
 script="$(basename "$0")"
-if [ "$script" = "copydownhere" ] || [ "$script" = "copydownhere.sh" ]; then
-    cmd="cp"
+if [[ "$script" == *"here" ]]; then
+    cmd="mv"
+    if [ "${script:0:4}" = "copy" ]; then
+        cmd="cp"
+    fi
+
+    dest="."
+    if [ "$1" != "" ]; then
+        dest="$1"
+    fi
 fi
-here="."
-if [ "$1" != "" ]; then
-    here="$1"
+if [[ "$script" == *"doc"* ]]; then
+    source="$HOME/Documents"
 fi
-there="$HOME/Downloads"
-last="$(ls -t1 "$there" | head -1)"
-"$cmd" "$there/$last" "$here"
-if [ -f "$here" ]; then
-    echo "$here"  # renamed
+
+last="$(ls -t1 "$source" | head -1)"
+
+if [ "$dest" != "" ]; then
+    "$cmd" "$source/$last" "$dest"
+
+    # Print the name of the file copied
+    if [ -f "$dest" ]; then
+        echo "$dest"  # renamed
+    else
+        echo "$last"
+    fi
 else
-    echo "$here/$last"
+    "$cmd" "$source/$last"
 fi
